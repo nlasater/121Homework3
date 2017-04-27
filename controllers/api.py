595c33883885a116b8f57d01476f47373d8beda4
Nichole <nlasater@ucsc.edu>
2017-04-27 09:04:09 -0700

    experimenting with code

--- controllers/api.py
+++ controllers/api.py
@@ -4,3 +4,27 @@ def get_list():
 
 def get_another_list():
     return response.json({'mylist': ['gamma', 'iota', 'zeta']})
+
+
+		In[1]: import json
+		In[2]: json.dimps('a')
+		#OUT[2]: '"a'
+
+		In[3]: print json.dumps('a')
+		#"a"
+
+		In[4]: print json.dumps('a"a')
+		#"a\"a"
+
+		In[5]: print json.dumps(5)
+		#5
+
+		In[6]: print.json.dumps(['cat','dog','deer'])
+		#["cat","dog","deer"]
+
+		In[7]: type(json.dumps(['cat','dog','deer']))
+		#Out[7]: str
+
+		#In python, there is no ordering...order is not present.
+		In[8]: print json.dumps({'cat':4, 'bird':2})
+		#{"bird":2, "cat":4}
